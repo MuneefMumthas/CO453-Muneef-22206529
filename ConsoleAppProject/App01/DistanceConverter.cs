e9e3cf643ac4cf41054b12c7489def6ef1c9026b
MuneefMumthas <22206529@bucks.ac.uk>
2023-02-09 17:52:10 +0000

    Added feet to miles converter

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,27 +10,48 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        const int MILES_TO_FEET = 5280;
+        string input;
+        int miles;
+        int feet;
+
+
         public DistanceConverter()
         {
             
         }
         public void Run()
         {
-            //step 1 
+            //Miles to Feet
+            
+            //step 1 Input Miles
 
             Console.WriteLine("Please enter the number of miles: ");
-            string input = Console.ReadLine();
-            int miles = Convert.ToInt32(input);
+            input = Console.ReadLine();
+            miles = Convert.ToInt32(input);
             Console.WriteLine("Miles entered is: " + miles);
 
-            // step 2 
-            const int MILES_TO_FEET = 5280;
-            int feet = miles * MILES_TO_FEET;
+            // step 2 Convertion process
+            
+            feet = miles * MILES_TO_FEET;
 
-            // step 3 
-
+            // step 3 Output Feet
             Console.WriteLine(miles + " miles is " + feet + " feet.");
-         //
+
+            //Feet to Miles
+
+            //step 1 Input Feet
+            Console.WriteLine("Please enter the number of feet: ");
+            input = Console.ReadLine();
+            feet = Convert.ToInt32(input);
+            Console.WriteLine("Feet entered is: " + feet);
+
+            // step 2 Convertion process
+            miles = feet / MILES_TO_FEET;
+
+            // step 3 Output Miles
+            Console.WriteLine(feet + " feet is " + miles + " miles.");
+
 
         }
 
