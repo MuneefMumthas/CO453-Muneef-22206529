f9b1b0c29d9c2ae538ba7fc064295b604e26c704
MuneefMumthas <22206529@bucks.ac.uk>
2023-02-17 00:19:51 +0000

    added prompt to choose apps

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -23,14 +23,47 @@ namespace ConsoleAppProject
             Console.WriteLine();
             Console.WriteLine(" =================================================");
             Console.WriteLine("    BNU CO453 Applications Programming 2022-2023! ");
-            Console.WriteLine("            Distance Unit converter               ");
+            Console.WriteLine("                App01 to App05                    ");
             Console.WriteLine("        By: Muneef Mumthas - 22206529             ");
             Console.WriteLine(" =================================================");
             Console.WriteLine();
 
-            DistanceConverter converter = new DistanceConverter();
-            converter.Run();
+            Console.WriteLine("Which application would you like to run? (1-2)");
+            Console.WriteLine("1. Distance Converter");
+            Console.WriteLine("2. BMI Calculator");
 
+            ConsoleKeyInfo keyPressed;
+            do
+            {
+                keyPressed = Console.ReadKey(true);
+            } while (keyPressed.Key != ConsoleKey.D1 && keyPressed.Key != ConsoleKey.D2);
+
+            int appNumber = 0;
+            switch (keyPressed.Key)
+            {
+                case ConsoleKey.D1:
+                    appNumber = 1;
+                    break;
+                case ConsoleKey.D2:
+                    appNumber = 2;
+                    break;
+            }
+
+            if (appNumber == 1)
+            {
+                DistanceConverter converter = new DistanceConverter();
+                converter.Run();
+            }
+            else if (appNumber == 2)
+            {
+                BMI bmiCalculator = new BMI();
+                bmiCalculator.Run();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Press any key to exit.");
+                Console.ReadKey();
+            }
         }
     }
 }
