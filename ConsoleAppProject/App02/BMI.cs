ea8e7bbed12116e6dd87da1ab9f8a485eac05050
MuneefMumthas <22206529@bucks.ac.uk>
2023-02-17 00:23:23 +0000

    added prompts to choose apps and started App02

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleAppProject.App02
+﻿using System;
+
+namespace ConsoleAppProject.App02
 {
     /// <summary>
     /// Please describe the main features of this App
@@ -7,6 +9,33 @@
     /// Student Name version 0.1
     /// </author>
     public class BMI
-    {
+    {private double weight;
+        private double height;
+        private double bmi;
+
+        public void Run()
+        {
+            Console.WriteLine("BMI Calculator\n");
+
+            InputWeight();
+            InputHeight();
+
+
+        }
+
+        private void InputWeight()
+        {
+            Console.Write("Enter your weight in kilograms: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+        }
+
+        private void InputHeight()
+        {
+            Console.Write("Enter your height in meters: ");
+            height = Convert.ToDouble(Console.ReadLine());
+        }
+
+
+        }
     }
-}
+
