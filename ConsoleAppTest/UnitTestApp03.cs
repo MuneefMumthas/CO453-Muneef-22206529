f36a434c81691bd1f6c0cc7f77a7b0864d28403d
MuneefMumthas <22206529@bucks.ac.uk>
2023-03-11 18:06:58 +0000

    Added Gardes unit testing for App03

--- ConsoleAppTest/UnitTestApp03.cs
+++ ConsoleAppTest/UnitTestApp03.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppTest
         StudentGrades studentGrades = new StudentGrades();
 
         [TestMethod]
-        public void TestInputMark()
+        public void Test_InputMark_With_Valid_Input()
         {
             using (StringReader stringReader = new StringReader("75\n"))
             {
@@ -21,6 +21,59 @@ namespace ConsoleAppTest
 
                 Assert.AreEqual(75, mark);
             }
+        }
+
+        [TestMethod]
+        public void Test_InputMark_With_Invalid_Input()
+        {
+            using (StringReader stringReader = new StringReader("abc\n50\n"))
+            {
+                Console.SetIn(stringReader);
+
+                int mark = studentGrades.InputMark("Enter mark for student 1: ");
+
+                Assert.AreEqual(50, mark);
+            }
+        }
+
+        [TestMethod]
+        public void Test_ClassifyGrade_With_Mark_Above_70()
+        {
+            string grade = studentGrades.ClassifyGrade(85);
+
+            Assert.AreEqual("- A (First Class)", grade);
+        }
+
+        [TestMethod]
+        public void Test_ClassifyGrade_With_Mark_Between_60_And_69()
+        {
+            string grade = studentGrades.ClassifyGrade(65);
+
+            Assert.AreEqual("- B (Upper Second Class)", grade);
+        }
+
+        [TestMethod]
+        public void Test_ClassifyGrade_With_Mark_Between_50_And_59()
+        {
+            string grade = studentGrades.ClassifyGrade(55);
+
+            Assert.AreEqual("- C (Lower Second Class)", grade);
+        }
+
+        [TestMethod]
+        public void Test_ClassifyGrade_With_Mark_Between_40_And_49()
+        {
+            string grade = studentGrades.ClassifyGrade(45);
+
+            Assert.AreEqual("- D (Third Class)", grade);
+        }
+
+        [TestMethod]
+        public void Test_ClassifyGrade_With_Mark_Bellow_40()
+        {
+            string grade = studentGrades.ClassifyGrade(10);
+
+            Assert.AreEqual("- F (Fail)", grade);
         }
     }
 }
